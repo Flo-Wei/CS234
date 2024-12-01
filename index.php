dd52645ac6a3144f93e3abec985ca7b883868353
Florian Weigelt <florian@weige.lt>
2024-11-30 23:03:28 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Library</title>
+    </head>
+    <body>
+        <h1>Library:</h1>
+        <ul>
+            <li>Book 1</li>
+            <li>Book 2</li>
+            <li>Book 3</li>
+        </ul>
+    </body>
+</html>
