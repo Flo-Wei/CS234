d41851ef26a41f766e79092a3109d998494709e1
Florian Weigelt <florian@weige.lt>
2024-12-02 22:54:07 -0600

    added project description

--- index.php
+++ index.php
@@ -31,6 +31,14 @@
         <h1 class="w3-text-teal w3-center">Welcome to the Homepage</h1>
         <p class="w3-text-grey w3-center"> Logged in as: <strong><?php echo htmlspecialchars($_SESSION["Username"]); ?></strong> | Role: <strong><?php echo htmlspecialchars($role); ?></strong></p>
         <p class="w3-text-grey w3-center">Explore the library, manage books, or access your favorite books!</p>
+        <p><br><br><br></p>
+        <p class="w3-text-grey w3-center">This is the Final Project for CS234 by Florian Weigelt<br>
+        The Project course of this project can be viewed on <a href="https://github.com/Flo-Wei/CS234-Project" target="_blank">Github</a></p>
+        <p class="w3-text-grey w3-center">
+            This is the database configuration:<br>
+            <img src="other/DatabaseStructure.png" alt="database" width="500">
+        </p>
+
     </div>
 </body>
 </html>
